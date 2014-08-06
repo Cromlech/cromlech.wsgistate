8332aefa6eca8372d4130693943fc648f874e387
Souheil CHELFOUH <trollfot@gmail.com>
2014-08-06 19:52:41 +0200

    Forwarding the local_conf options to the decorator, for further configuration

--- src/cromlech/wsgistate/middleware.py
+++ src/cromlech/wsgistate/middleware.py
@@ -8,13 +8,13 @@ from .controlled import WsgistateSession
 
 
 def session_wrapper(app, *global_conf, **local_conf):
-    session_key = local_conf.get('session_key', 'session')
-    wrapper = session_decorator(key=session_key)
+    session_key = local_conf.pop('session_key', 'session')
+    wrapper = session_decorator(key=session_key, **local_conf)
     return wrapper(app)
 
 
 def file_session_wrapper(app, *global_conf, **local_conf):
-    session_key = local_conf.get('session_key', 'session')
+    session_key = local_conf.pop('session_key', 'session')
     path = local_conf.get('session_cache', tempfile.gettempdir())
-    wrapper = file_session_decorator(path, key=session_key)
+    wrapper = file_session_decorator(path, key=session_key, **local_conf)
     return wrapper(app)
