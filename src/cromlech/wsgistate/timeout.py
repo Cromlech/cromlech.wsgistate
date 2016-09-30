8e808f4162fbc25333ed1cd1ff84a485bd5e798f
Souheil CHELFOUH <trollfot@gmail.com>
2016-09-30 19:06:52 +0200

    Updated timeout code, to work propperly. It needed a new session manager to handle the exception.

--- src/cromlech/wsgistate/timeout.py
+++ src/cromlech/wsgistate/timeout.py
@@ -25,22 +25,30 @@ class TimeoutFileCache(FileCache):
             # Remove item if time has expired.
             if exp < time.time():
                 self.delete(key)
-                raise TimeoutException('Timeout', default)
+                raise TimeoutException('Timeout', value)
             return value
         except (IOError, OSError, EOFError, pickle.PickleError):
             pass
         return default
 
 
+class Manager(SessionManager):
+
+    def _get(self, environ):
+        try:
+            return SessionManager._get(self, environ)
+        except TimeoutException as timeout:
+            self._sid, self.session = self._cache.create()
+            self.new = True
+            self.expired = True
+            environ['session.timeout'] = timeout
+    
+
 class TimeoutCookieSession(CookieSession):
 
     def __call__(self, environ, start_response):
         # New session manager instance each time
-        try:
-            sess = SessionManager(self.cache, environ, **self.kw)
-        except TimeoutException as timeout:
-            sess = timeout.default
-            environ['session.timeout'] = timeout
+        sess = Manager(self.cache, environ, **self.kw)
         environ[self.key] = sess
         try:
             # Return initial response if new or session id is random
